ab99d84ac0f14cf180bf0b3d0a174b63b9851fb1
Athiqur01 <athik.apece@gmail.com>
2024-04-30 03:31:42 +0600

    user update

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ async function run() {
 
     app.post("/items", async (req,res)=>{
         const newItems=req.body
-        console.log(newItems)
+        
         const result= await itemsCollection.insertOne(newItems) 
         res.send(result)
     })
